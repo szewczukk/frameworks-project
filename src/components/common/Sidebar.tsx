535bb766f7d4fbaabc075edae132e644b8c97b39
szewczukk <dmitro.szewczuk@gmail.com>
2023-10-30 15:27:35 +0100

    Create simple Sidebar component

---
+++ src/components/common/Sidebar.tsx
@@ -0,0 +1,13 @@
+export default function Sidebar() {
+	return (
+		<aside className="h-full w-72 bg-slate-300 py-4">
+			<ul>
+				<a href="">
+					<li className="px-4 py-2 transition-colors hover:bg-slate-500 hover:text-gray-50">
+						Home
+					</li>
+				</a>
+			</ul>
+		</aside>
+	);
+}
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,3 +1,12 @@
+import Sidebar from '../common/Sidebar';
+
 export default function Home() {
-	return <h1>Hello, world!</h1>;
+	return (
+		<div className="flex h-screen items-start">
+			<Sidebar />
+			<div className="p-4">
+				<h1>Hello, world!</h1>
+			</div>
+		</div>
+	);
 }
