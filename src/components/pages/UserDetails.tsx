52d2e01c21060978605a22f50cfa3fa178290b6b
szewczukk <dmitro.szewczuk@gmail.com>
2023-10-30 20:46:20 +0100

    Fetch data for a user

--- src/components/pages/UserDetails.tsx
+++ src/components/pages/UserDetails.tsx
@@ -1,3 +1,30 @@
+import { useQuery, useQueryClient } from 'react-query';
+import Sidebar, { User } from '../common/Sidebar';
+import api from '../../lib/api';
+import { useParams } from 'react-router-dom';
+
 export default function UserDetails() {
-	return <h1>User Details</h1>;
+	const params = useParams();
+	const queryClient = useQueryClient();
+	const { isLoading, data, error } = useQuery<User>({
+		queryKey: ['users', params.id],
+		queryFn: () => api.get(`/users/${params.id}/`).then((res) => res.data),
+	});
+
+	if (isLoading) {
+		return <h1>Loading..</h1>;
+	}
+
+	if (error || !data) {
+		return <h1>Error</h1>;
+	}
+
+	return (
+		<div className="flex h-screen items-start">
+			<Sidebar users={queryClient.getQueryData(['users'])!} />
+			<div className="p-6">
+				<h1>Hello, {data.username}!</h1>
+			</div>
+		</div>
+	);
 }
