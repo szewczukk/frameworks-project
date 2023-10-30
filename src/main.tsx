6f45fad4ab8a53aa35d145ed5d967a5dd06c325a
szewczukk <dmitro.szewczuk@gmail.com>
2023-10-30 15:18:08 +0100

    Create router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
+import Home from './components/pages/Home';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 );
