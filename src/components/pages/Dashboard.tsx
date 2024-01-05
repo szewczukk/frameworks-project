b3e2a6a2629f4585d27e1191275cd60808a322df
szewczukk <dmitro.szewczuk@gmail.com>
2024-01-05 19:44:59 +0100

    Load todos and albums

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -29,9 +29,28 @@ const userSchema = z.object({
 
 type User = z.infer<typeof userSchema>;
 
+const albumSchema = z.object({
+	id: z.number(),
+	title: z.string(),
+	userId: z.number(),
+});
+
+type Album = z.infer<typeof albumSchema>;
+
+const todoSchema = z.object({
+	id: z.number(),
+	title: z.string(),
+	completed: z.boolean(),
+	userId: z.number(),
+});
+
+type Todo = z.infer<typeof todoSchema>;
+
 export default function DashboardPage() {
 	const { userId } = useAuthContext();
 	const [user, setUser] = useState<User | undefined>();
+	const [albums, setAlbums] = useState<Album[]>([]);
+	const [todos, setTodos] = useState<Todo[]>([]);
 
 	useEffect(() => {
 		(async () => {
@@ -40,13 +59,54 @@ export default function DashboardPage() {
 
 			setUser(responseUser);
 		})();
+
+		(async () => {
+			const result = await api.get(`/users/${userId}/albums`);
+			const responseAlbums = z.array(albumSchema).parse(result.data);
+
+			setAlbums(responseAlbums);
+		})();
+
+		(async () => {
+			const result = await api.get(`/users/${userId}/todos`);
+			const responseTodos = z.array(todoSchema).parse(result.data);
+
+			setTodos(responseTodos);
+		})();
 	}, []);
 
 	return (
-		<div>
+		<div className="container mx-auto mt-8 bg-slate-200 p-8">
 			{user ? (
 				<>
-					<h1>Hello, {user.name}</h1>
+					<h1 className="text-2xl font-semibold">{user.name}</h1>
+					<div className="flex flex-col flex-wrap gap-4 md:flex-row">
+						<div className="flex-1">
+							<h2 className="text-xl font-semibold">Albums</h2>
+							<ul>
+								{albums.map((album) => (
+									<li key={album.id} className="list-inside list-disc">
+										{album.title}
+									</li>
+								))}
+							</ul>
+						</div>
+						<div className="flex-1">
+							<h2 className="text-xl font-semibold">Todos</h2>
+							<ul>
+								{todos.map((todo) => (
+									<li
+										key={todo.id}
+										className={`list-inside list-disc ${
+											todo.completed && 'line-through'
+										}`}
+									>
+										{todo.title}
+									</li>
+								))}
+							</ul>
+						</div>
+					</div>
 				</>
 			) : (
 				<p>Loading..</p>
