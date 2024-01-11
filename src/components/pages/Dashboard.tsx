29a78176ed268c4e1ad09e068cc8fd81d72797a3
szewczukk <dmitro.szewczuk@gmail.com>
2024-01-11 13:24:44 +0100

    Refactor Dashboard

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,63 +1,11 @@
-import { z } from 'zod';
+import { User, userSchema } from '@/lib/types';
 import { useAuthContext } from '../contexts/AuthContext';
 import { useEffect, useState } from 'react';
 import api from '@/lib/api';
-import CreateTodoForm, { TodoFormValues } from '../Dashboard/CreateTodoForm';
-
-const userSchema = z.object({
-	id: z.number(),
-	name: z.string(),
-	username: z.string(),
-	email: z.string(),
-	address: z.object({
-		street: z.string(),
-		suite: z.string(),
-		city: z.string(),
-		zipcode: z.string(),
-		geo: z.object({
-			lat: z.string(),
-			lng: z.string(),
-		}),
-	}),
-	phone: z.string(),
-	website: z.string(),
-	company: z.object({
-		name: z.string(),
-		catchPhrase: z.string(),
-		bs: z.string(),
-	}),
-});
-
-type User = z.infer<typeof userSchema>;
-
-const albumSchema = z.object({
-	id: z.number(),
-	title: z.string(),
-	userId: z.number(),
-});
-
-type Album = z.infer<typeof albumSchema>;
-
-const createTodoSchema = z.object({
-	id: z.number(),
-	title: z.string(),
-	userId: z.string(),
-});
-
-const todoSchema = z.object({
-	id: z.number(),
-	title: z.string(),
-	completed: z.boolean(),
-	userId: z.number(),
-});
-
-type Todo = z.infer<typeof todoSchema>;
 
 export default function DashboardPage() {
 	const { userId } = useAuthContext();
 	const [user, setUser] = useState<User | undefined>();
-	const [albums, setAlbums] = useState<Album[]>([]);
-	const [todos, setTodos] = useState<Todo[]>([]);
 
 	useEffect(() => {
 		(async () => {
@@ -66,73 +14,41 @@ export default function DashboardPage() {
 
 			setUser(responseUser);
 		})();
-
-		(async () => {
-			const result = await api.get(`/users/${userId}/albums`);
-			const responseAlbums = z.array(albumSchema).parse(result.data);
-
-			setAlbums(responseAlbums);
-		})();
-
-		(async () => {
-			const result = await api.get(`/users/${userId}/todos`);
-			const responseTodos = z.array(todoSchema).parse(result.data);
-
-			setTodos(responseTodos);
-		})();
 	}, []);
 
-	const handleSubmitTodoForm = async (values: TodoFormValues) => {
-		const result = await api.post(`/users/${userId}/todos`, values);
-		const responseTodo = createTodoSchema.parse(result.data);
-
-		setTodos((prev) => [
-			...prev,
-			{
-				...responseTodo,
-				completed: false,
-				userId: parseInt(responseTodo.userId),
-			},
-		]);
-	};
+	if (!user) {
+		return <h1>No user found!</h1>;
+	}
 
 	return (
 		<div className="container mx-auto mt-8 bg-slate-200 p-8">
-			{user ? (
-				<>
-					<h1 className="text-2xl font-semibold">{user.name}</h1>
-					<div className="flex flex-col flex-wrap gap-4 md:flex-row">
-						<div className="flex flex-1 flex-col gap-8">
-							<h2 className="text-xl font-semibold">Albums</h2>
-							<ul>
-								{albums.map((album) => (
-									<li key={album.id} className="list-inside list-disc">
-										{album.title}
-									</li>
-								))}
-							</ul>
-						</div>
-						<div className="flex flex-1 flex-col gap-8">
-							<h2 className="text-xl font-semibold">Todos</h2>
-							<ul>
-								{todos.map((todo) => (
-									<li
-										key={todo.id}
-										className={`list-inside list-disc ${
-											todo.completed && 'line-through'
-										}`}
-									>
-										{todo.title}
-									</li>
-								))}
-							</ul>
-							<CreateTodoForm onSubmit={handleSubmitTodoForm} />
-						</div>
-					</div>
-				</>
-			) : (
-				<p>Loading..</p>
-			)}
+			<h1 className="mb-8 text-xl font-semibold">{user.name}</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Name</th>
+						<th>Username</th>
+						<th>Website</th>
+						<th>Phone</th>
+						<th>Email</th>
+						<th>Address</th>
+						<th>Company</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>{user.id}</td>
+						<td>{user.name}</td>
+						<td>{user.username}</td>
+						<td>{user.website}</td>
+						<td>{user.phone}</td>
+						<td>{user.email}</td>
+						<td>{user.address.city}</td>
+						<td>{user.company.name}</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	);
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,5 @@
 import { z } from 'zod';
 
-export const userSchema = z.object({
-	id: z.number(),
-	name: z.string(),
-	username: z.string(),
-	email: z.string(),
-});
-
 export const postSchema = z.object({
 	id: z.number(),
 	userId: z.number(),
@@ -26,6 +19,30 @@ export const commentSchema = z.object({
 	name: z.string(),
 	email: z.string(),
 	body: z.string(),
+});
+
+export const userSchema = z.object({
+	id: z.number(),
+	name: z.string(),
+	username: z.string(),
+	email: z.string(),
+	address: z.object({
+		street: z.string(),
+		suite: z.string(),
+		city: z.string(),
+		zipcode: z.string(),
+		geo: z.object({
+			lat: z.string(),
+			lng: z.string(),
+		}),
+	}),
+	phone: z.string(),
+	website: z.string(),
+	company: z.object({
+		name: z.string(),
+		catchPhrase: z.string(),
+		bs: z.string(),
+	}),
 });
 
 export type User = z.infer<typeof userSchema>;
